07d686d357cb926816e0960cb27a7677c1b2b71d
Anthony Wijnen <Anthony@Anthonys-MBP2014.local>
2015-10-08 20:42:34 +0200

    complete landing page html & css

---
+++ app/client/templates/explain_basic_income/explain_basic_income.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* ExplainBasicIncome: Event Handlers */
+/*****************************************************************************/
+Template.ExplainBasicIncome.events({
+});
+
+/*****************************************************************************/
+/* ExplainBasicIncome: Helpers */
+/*****************************************************************************/
+Template.ExplainBasicIncome.helpers({
+});
+
+/*****************************************************************************/
+/* ExplainBasicIncome: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ExplainBasicIncome.onCreated(function () {
+});
+
+Template.ExplainBasicIncome.onRendered(function () {
+});
+
+Template.ExplainBasicIncome.onDestroyed(function () {
+});
--- app/lib/routes.js
+++ app/lib/routes.js
@@ -10,3 +10,17 @@ Router.route('/', {
   controller: 'HomeController',
   where: 'client'
 });
+
+Router.route('/votes/verify', {
+	name: 'votes.verify',
+	controller: 'VotesController',
+	action: 'verify',
+	where: 'client',
+})
+
+Router.route('/votes/verify', {
+	name: 'votes.thanks',
+	controller: 'VotesController',
+	action: 'thanks',
+	where: 'client',
+})
